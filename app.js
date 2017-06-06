e9df5117f0395bc515b668ab405cde45258b70f1
Daniel Torres <daniel.torres@sheeper.com>
2017-06-06 04:35:43 -0300

    [Farm] Implementing basic operations

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var Express = require('express');
 var Log4js = require('log4js');
+var BodyParser = require('body-parser');
 
 var fs = require('fs');
 var configObject = JSON.parse(fs.readFileSync(__dirname + '/config/config.json', 'utf8'));
@@ -15,7 +16,11 @@ Logger.setLevel('DEBUG');
 
 var FarmService = require('./lib/farm.service');
 
+
 var App = new Express();
+
+App.use(BodyParser.urlencoded({ extended: true }));
+App.use(BodyParser.json());
 
 App.get('/', function (req, res) {
     res.send('Treevia Core Service is up!');
@@ -24,6 +29,24 @@ App.get('/', function (req, res) {
 App.get('/farms', function (req, res) {
     FarmService.getAll(function (farms) {
     	res.send(farms);
+    });
+});
+
+App.get('/farms/:id', function (req, res) {
+    FarmService.get(req.params.id, function (farm) {
+    	res.send(farm);
+    });
+});
+
+App.post('/farms', function (req, res) {
+    FarmService.add(req.body, function (id) {
+    	res.send(id);
+    });
+});
+
+App.delete('/farms/:id', function (req, res) {
+    FarmService.remove(req.params.id, function (deletedFarm) {
+    	res.send(deletedFarm);
     });
 });
 
